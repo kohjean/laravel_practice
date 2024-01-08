6004c42ef7a7a5cc35c7873df3fd40eed8b5a17a
kohjean <58461459+kohjean@users.noreply.github.com>
2024-01-08 20:07:01 +0900

    Ts + React の導入

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,23 @@
-import { defineConfig, loadEnv } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig, loadEnv } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import react from '@vitejs/plugin-react'
 
 export default ({ mode }) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
   return defineConfig({
     plugins: [
+      react(),
       laravel({
-        input: [
-          'resources/css/app.css',
-          'resources/js/app.js',
-        ],
+        input: ['resources/css/app.css', 'resources/ts/index.tsx'],
         refresh: true,
       }),
     ],
     server: {
       host: true,
       hmr: {
-        host: process.env.VITE_PUSHER_HOST
-      }
+        host: process.env.VITE_PUSHER_HOST,
+      },
     },
   })
-};
+}
